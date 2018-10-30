08f9c597bfc7c94429eab56a39170e2880f7a8be
jasonoochen <chjchjason@gmail.com>
2018-10-30 16:46:06 -0700

    one to many

--- src/com/jason/hibernate/entities/manyToOne/both/HibernateTest.java
+++ src/com/jason/hibernate/entities/manyToOne/both/HibernateTest.java
@@ -1,18 +1,9 @@
 package com.jason.hibernate.entities.manyToOne.both;
 
-import java.io.InputStream;
-import java.sql.Blob;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Date;
-
-import org.hibernate.Hibernate;
-import org.hibernate.LazyInitializationException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.jdbc.Work;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 import org.junit.After;
@@ -45,10 +36,22 @@ public class HibernateTest {
 	}
 	
 	@Test
+	public void testCascade(){
+		Customer customer = (Customer) session.get(Customer.class, 3);
+		customer.getOrders().clear();
+	}
+	
+	@Test
 	public void testDelete(){
-		//在不设定级联关系的情况下, �? 1 这一端的对象�? n 的对象在引用, 不能直接删除 1 这一端的对象
+		//在不设定级联关系的情况下, 且 1 这一端的对象有 n 的对象在引用, 不能直接删除 1 这一端的对象
 		Customer customer = (Customer) session.get(Customer.class, 1);
 		session.delete(customer); 
+	}
+	
+	@Test
+	public void testUpdat2(){
+		Customer customer = (Customer) session.get(Customer.class, 1);
+		customer.getOrders().iterator().next().setOrderName("GGG"); 
 	}
 	
 	@Test
@@ -58,9 +61,26 @@ public class HibernateTest {
 	}
 	
 	@Test
+	public void testOne2ManyGet(){
+		//1. 对 n 的一端的集合使用延迟加载
+		Customer customer = (Customer) session.get(Customer.class, 7);
+		System.out.println(customer.getCustomerName()); 
+		//2. 返回的多的一端的集合时 Hibernate 内置的集合类型. 
+		//该类型具有延迟加载和存放代理对象的功能. 
+		System.out.println(customer.getOrders().getClass()); 
+		
+		//session.close();
+		//3. 可能会抛出 LazyInitializationException 异常 
+		
+		System.out.println(customer.getOrders().size()); 
+		
+		//4. 再需要使用集合中元素的时候进行初始化. 
+	}
+	
+	@Test
 	public void testMany2OneGet(){
-		//1. 若查询多的一端的�?个对�?, 则默认情况下, 只查询了多的�?端的对象. 而没有查询关联的
-		//1 的那�?端的对象!
+		//1. 若查询多的一端的一个对象, 则默认情况下, 只查询了多的一端的对象. 而没有查询关联的
+		//1 的那一端的对象!
 		Order order = (Order) session.get(Order.class, 1);
 		System.out.println(order.getOrderName()); 
 		
@@ -72,44 +92,47 @@ public class HibernateTest {
 		Customer customer = order.getCustomer();
 		System.out.println(customer.getCustomerName()); 
 		
-		//3. 在查�? Customer 对象�?, 由多的一端导航到 1 的一端时, 
-		//若此�? session 已被关闭, 则默认情况下
-		//会发�? LazyInitializationException 异常
+		//3. 在查询 Customer 对象时, 由多的一端导航到 1 的一端时, 
+		//若此时 session 已被关闭, 则默认情况下
+		//会发生 LazyInitializationException 异常
 		
-		//4. 获取 Order 对象�?, 默认情况�?, 其关联的 Customer 对象是一个代理对�?!
+		//4. 获取 Order 对象时, 默认情况下, 其关联的 Customer 对象是一个代理对象!
 		
 	}
 	
 	@Test
 	public void testMany2OneSave(){
 		Customer customer = new Customer();
-		customer.setCustomerName("BB");
+		customer.setCustomerName("AA");
 		
 		Order order1 = new Order();
-		order1.setOrderName("ORDER-3");
+		order1.setOrderName("ORDER-1");
 		
 		Order order2 = new Order();
-		order2.setOrderName("ORDER-4");
+		order2.setOrderName("ORDER-2");
 		
 		//设定关联关系
 		order1.setCustomer(customer);
 		order2.setCustomer(customer);
 		
-		//执行  save 操作: 先插�? Customer, 再插�? Order, 3 �? INSERT
-		//先插�? 1 的一�?, 再插�? n 的一�?, 只有 INSERT 语句.
-//		session.save(customer);
-//		
+		customer.getOrders().add(order1);
+		customer.getOrders().add(order2);
+		
+		//执行  save 操作: 先插入 Customer, 再插入 Order, 3 条 INSERT, 2 条 UPDATE
+		//因为 1 的一端和 n 的一端都维护关联关系. 所以会多出 UPDATE
+		//可以在 1 的一端的 set 节点指定 inverse=true, 来使 1 的一端放弃维护关联关系!
+		//建议设定 set 的 inverse=true, 建议先插入 1 的一端, 后插入多的一端
+		//好处是不会多出 UPDATE 语句
+		session.save(customer);
+		
 //		session.save(order1);
 //		session.save(order2);
 		
-		//先插�? Order, 再插�? Customer. 3 �? INSERT, 2 �? UPDATE
-		//先插�? n 的一�?, 再插�? 1 的一�?, 会多�? UPDATE 语句!
-		//因为在插入多的一端时, 无法确定 1 的一端的外键�?. �?以只能等 1 的一端插入后, 再额外发�? UPDATE 语句.
-		//推荐先插�? 1 的一�?, 后插�? n 的一�?
-		session.save(order1);
-		session.save(order2);
-		
-		session.save(customer);
+		//先插入 Order, 再插入 Cusomer, 3 条 INSERT, 4 条 UPDATE
+//		session.save(order1);
+//		session.save(order2);
+//		
+//		session.save(customer);
 	}
 
 }
